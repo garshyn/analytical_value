a514d6e5bb734715a76e8c8ec3db4f12ec7eccde
Andrew Garshyn <andrew@ethicontrol.com>
2019-02-21 18:01:53 +0200

    dynamics hash to return only selected keys

--- lib/analytical_value/dynamics_hash.rb
+++ lib/analytical_value/dynamics_hash.rb
@@ -2,19 +2,21 @@ module AnalyticalValue
   class DynamicsHash
     delegate :[], to: :result
 
-    def initialize(prev_hash, current_hash)
+    def initialize(prev_hash, current_hash, keys: nil)
       @prev_hash = prev_hash
       @current_hash = current_hash
+      @selected_keys = default_keys
+      @selected_keys = default_keys & keys if keys.present?
     end
 
     def result
-      @_result ||= keys.each_with_object({}) do |key, hash|
+      @_result ||= selected_keys.each_with_object({}) do |key, hash|
         hash[key] = Dynamics.new to_hash[key].first, to_hash[key].last
       end
     end
 
     def to_hash
-      @_hash ||= keys.each_with_object(Hash.new(0)) do |key, hash|
+      @_hash ||= selected_keys.each_with_object(Hash.new(0)) do |key, hash|
         hash[key] = [
           prev_hash.fetch(key, 0),
           current_hash.fetch(key, 0),
@@ -24,10 +26,10 @@ module AnalyticalValue
 
     protected
 
-    attr_reader :prev_hash, :current_hash
+    attr_reader :prev_hash, :current_hash, :selected_keys
 
-    def keys
-      @_keys ||= prev_hash.merge(current_hash).keys
+    def default_keys
+      @_default_keys ||= prev_hash.merge(current_hash).keys
     end
   end
 end
--- spec/analytical_value/dynamics_hash_spec.rb
+++ spec/analytical_value/dynamics_hash_spec.rb
@@ -30,5 +30,37 @@ module AnalyticalValue
         3 => [0, 4],
       )
     end
+
+    it 'selects keys' do
+      hash_1 = {
+        1 => 1,
+        2 => 2,
+      }
+      hash_2 = {
+        1 => 3,
+        3 => 4,
+      }
+      hash = described_class.new(
+        hash_1,
+        hash_2,
+        keys: [1, 2],
+      )
+
+      dynamics_1 = Dynamics.new(1, 3)
+      dynamics_2 = Dynamics.new(2, 0)
+
+      expect(hash[1]).to eq dynamics_1
+      expect(hash[2]).to eq dynamics_2
+      expect(hash[3]).to be_nil
+      expect(hash.result).to eq(
+        1 => dynamics_1,
+        2 => dynamics_2,
+      )
+
+      expect(hash.to_hash).to eq(
+        1 => [1, 3],
+        2 => [2, 0],
+      )
+    end
   end
 end
